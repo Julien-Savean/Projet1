8709611c026d1962219a8fdea7354622d5d842ba
Benjamin Deneux <benjamindeneux@hotmail.com>
2021-09-30 00:53:18 +0200

    Update jeudi matin Header

---
+++ Planet.js
@@ -0,0 +1,27 @@
+let styleHeader = document.querySelector(".liHeader");
+let planeteMenu = document.querySelector("#menuPlanete");
+let styleHeader1 = document.querySelector(".liHeader1");
+let styleHeader2 = document.querySelector(".liHeader2");
+
+
+styleHeader.addEventListener('click', function() {
+    styleHeader.style.backgroundColor = "red";
+    
+})
+
+
+styleHeader1.addEventListener('click', function () {
+    styleHeader1.style.backgroundColor = "red";
+    planeteMenu.style.opacity = 1.0;
+    
+})
+
+
+styleHeader2.addEventListener('click', function() {
+    styleHeader2.style.backgroundColor = "red";
+    
+})
+
+
+
+
