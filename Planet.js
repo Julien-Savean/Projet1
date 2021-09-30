4a96850c2e5ccdccaa93e4ced205bd80f2ae1b23
Benjamin Deneux <benjamindeneux@hotmail.com>
2021-09-30 13:17:46 +0200

    Changement sur le header jeudi début d'aprem

--- Planet.js
+++ Planet.js
@@ -2,26 +2,32 @@ let styleHeader = document.querySelector(".liHeader");
 let planeteMenu = document.querySelector("#menuPlanete");
 let styleHeader1 = document.querySelector(".liHeader1");
 let styleHeader2 = document.querySelector(".liHeader2");
+let body = document.querySelector("#julien");
 
 
-styleHeader.addEventListener('click', function() {
-    styleHeader.style.backgroundColor = "red";
-    
-})
+styleHeader.addEventListener("click", function () {
+  styleHeader.style.backgroundColor = "red";
+});
 
-
-styleHeader1.addEventListener('click', function () {
+function slideOn() {
+  styleHeader1.addEventListener("click", function red() {
     styleHeader1.style.backgroundColor = "red";
-    planeteMenu.style.opacity = 1.0;
-    
-})
+    planeteMenu.style.opacity = 1;
+    body.style.opacity = 0;
+
+    styleHeader1.addEventListener("click", function () {
+      styleHeader1.style.backgroundColor = "white";
+      planeteMenu.style.opacity = 0;
+      body.style.opacity = 1;
+
+      return slideOn();
+    });
+  });
+}
+
+slideOn();
 
 
-styleHeader2.addEventListener('click', function() {
-    styleHeader2.style.backgroundColor = "red";
-    
-})
-
-
-
-
+styleHeader2.addEventListener("click", function () {
+  styleHeader2.style.backgroundColor = "red";
+});
