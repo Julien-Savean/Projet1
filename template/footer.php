4eb6008a6fc7cacf3c267f2612ee6801ba6b27c4
Julien Savéan <j.savean@gmail.com>
2021-10-07 09:20:04 +0200

    adds in database and video footer

--- template/footer.php
+++ template/footer.php
@@ -1,9 +1,9 @@
 <div class="footer">
 
     <ul id="ulFooter">
-        <li id="liFooter"><a href="">Email</a></li>
-        <li id="liFooter"><a href="">Linkedin</a></li>
         <li id="liFooter"><a href="https://github.com/Julien-Savean/Projet1">Github</a></li>
+        <li id="liFooter"><a href="https://www.linkedin.com/company/extraterrestrial-and-ufo-research-organisation-international/">Linkedin</a></li>
+        <li id="liFooter"><a href="https://www.youtube.com/embed/RRq_MvT33pA?start=6&rel=0&autoplay=1">Sources</a></li>
         <li>
             <p>👋🚀</p>
         </li>
